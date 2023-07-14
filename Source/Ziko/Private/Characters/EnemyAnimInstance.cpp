099058282b66c58dac2e3c0cce12149878b44952
yaelh <elhaeey@gmail.com>
2023-07-14 19:15:51 +0100

    update enemy animations

--- Source/Ziko/Private/Characters/EnemyAnimInstance.cpp
+++ Source/Ziko/Private/Characters/EnemyAnimInstance.cpp
@@ -3,8 +3,48 @@
 
 #include "Characters/EnemyAnimInstance.h"
 
+#include "Characters/AI/BaseEnemy.h"
+#include "Characters/Player/BaseCharacter.h"
+#include "Kismet/KismetMathLibrary.h"
 
-EnemyAnimInstance::EnemyAnimInstance()
+UEnemyAnimInstance::UEnemyAnimInstance()
 {
 }
 
+void UEnemyAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	Character = Cast<ABaseEnemy>(TryGetPawnOwner());
+	Target = Cast<ABaseCharacter>(GetWorld()->GetFirstPlayerController());
+	Aggro = false;
+}
+
+void UEnemyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if (Character)
+		UpdateAnimAttributes();
+	else
+		Character = Cast<ABaseEnemy>(TryGetPawnOwner());
+}
+
+void UEnemyAnimInstance::UpdateAnimAttributes()
+{
+	const FVector& Velocity = Character->GetVelocity();
+	
+	Speed = Velocity.Size();
+	RotationAngle = GetRotationAngle(Velocity);
+	//fix aggro anim
+	
+}
+
+float UEnemyAnimInstance::GetRotationAngle(const FVector& Velocity) const
+{
+	FVector Char = Character ->GetActorForwardVector();
+	float Angle = UKismetMathLibrary::Acos(FVector::DotProduct(Char, Velocity.GetSafeNormal()));
+	float Sign = FVector::CrossProduct(Char,Velocity.GetSafeNormal()).Z;
+	Angle = Angle * 180.0f/PI;
+	Sign<0? Angle = -Angle: Angle;
+	
+	return (Angle);
+}
--- Source/Ziko/Public/Characters/EnemyAnimInstance.h
+++ Source/Ziko/Public/Characters/EnemyAnimInstance.h
@@ -3,19 +3,46 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnemyAnimInstance.generated.h"
 
-/**
- * 
- */
+
+class ABaseCharacter;
+class ABaseEnemy;
+
 UCLASS()
 class ZIKO_API UEnemyAnimInstance: public UAnimInstance
 {
 	GENERATED_BODY()
+	
 public:
 	UEnemyAnimInstance();
+
+protected:
 	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 
 private:
 	void UpdateAnimAttributes();
+	float GetRotationAngle(const FVector& Velocity) const;
 
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation Properties")
+	ABaseCharacter* Target;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category="Animation Properties")
+	ABaseEnemy* Character;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation Properties")
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation Properties")
+	float RotationAngle;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation Properties")
+	bool Aggro;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation Properties")
+	bool Stunned;
+	
+	
 };
